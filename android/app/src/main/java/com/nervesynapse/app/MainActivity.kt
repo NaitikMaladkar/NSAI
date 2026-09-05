cbffb3cce7fed7a7afb425845d7752b5d02634cb
NaitikMaladkar <285917070+NaitikMaladkar@users.noreply.github.com>
2026-09-05 12:16:52 +0000

    fix(android): remove mainComponentName override (parent doesn't declare it in RN 0.84)
    ReactActivity in RN 0.84 no longer declares mainComponentName as an open property, so the override failed compilation.

--- android/app/src/main/java/com/nervesynapse/app/MainActivity.kt
+++ android/app/src/main/java/com/nervesynapse/app/MainActivity.kt
@@ -2,11 +2,8 @@ package com.nervesynapse.app
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 
 class MainActivity : ReactActivity() {
-  override val mainComponentName: String = "NerveSynapse"
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
   }
